ba37746b8e3b33708e2708c573acdef235d9199f
Kostya <koooooostaaa@gmail.com>
2023-12-22 10:55:52 +0200

    pr5_3

--- script/script.js
+++ script/script.js
@@ -3,7 +3,85 @@ document.addEventListener("DOMContentLoaded", function () {
   const modalBlock = document.querySelector("#modalBlock");
   const closeModal = document.querySelector("#closeModal");
   const questionTitle = document.querySelector("#question");
-  const fromAnswers = document.querySelector("#formAnswers");
+  const formAnswers = document.querySelector("#formAnswers");
+  const prevButton = document.querySelector("#prev");
+  const nextButton = document.querySelector("#next");
+  const sendButton = document.querySelector("#send");
+
+  const questions = [
+    {
+      question: "Какого цвета бургер?",
+      answers: [
+        {
+          title: "Стандарт",
+          url: "./image/burger.png",
+        },
+        {
+          title: "Черный",
+          url: "./image/burgerBlack.png",
+        },
+      ],
+      type: "radio",
+    },
+    {
+      question: "Из какого мяса котлета?",
+      answers: [
+        {
+          title: "Курица",
+          url: "./image/chickenMeat.png",
+        },
+        {
+          title: "Говядина",
+          url: "./image/beefMeat.png",
+        },
+        {
+          title: "Свинина",
+          url: "./image/porkMeat.png",
+        },
+      ],
+      type: "radio",
+    },
+    {
+      question: "Дополнительные ингредиенты?",
+      answers: [
+        {
+          title: "Помидор",
+          url: "./image/tomato.png",
+        },
+        {
+          title: "Огурец",
+          url: "./image/cucumber.png",
+        },
+        {
+          title: "Салат",
+          url: "./image/salad.png",
+        },
+        {
+          title: "Лук",
+          url: "./image/onion.png",
+        },
+      ],
+      type: "checkbox",
+    },
+    {
+      question: "Добавить соус?",
+      answers: [
+        {
+          title: "Чесночный",
+          url: "./image/sauce1.png",
+        },
+        {
+          title: "Томатный",
+          url: "./image/sauce2.png",
+        },
+        {
+          title: "Горчичный",
+          url: "./image/sauce3.png",
+        },
+      ],
+      type: "radio",
+    },
+  ];
 
   btnOpenModal.addEventListener("click", () => {
     modalBlock.classList.add("d-block");
@@ -15,30 +93,119 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   const playTest = () => {
-    const renderAnswers = (index) => {
-      questionTitle.textContent = "Какого цвета бургер вы хотите?";
+    let numberQuestion = 0;
+    let finalAnswers = [];
 
-      const images = ["./image/burger.png", "./image/burgerBlack.png"];
-      const titles = ["Стандарт", "Черный"];
+    const updateMarkup = () => {
+      switch (numberQuestion) {
+        case 0:
+          prevButton.classList.add("d-none");
+          nextButton.classList.remove("d-none");
+          sendButton.classList.add("d-none");
+          break;
+        case questions.length:
+          prevButton.classList.remove("d-none");
+          nextButton.classList.add("d-none");
+          sendButton.classList.remove("d-none");
 
-      fromAnswers.innerHTML = `
-            <div class="answers-item d-flex flex-column">
-            <input type="radio" id="answerItem1" name="answer" class="d-none">
-            <label for="answerItem1" class="d-flex flex-column justify-content-between">
-              <img class="answerImg" src="${images[0]}" alt="burger">
-              <span>${titles[0]}</span>
-            </label>
+          formAnswers.innerHTML = `
+          <div class="form-group">
+            <label for="numberPhone">Enter your number</label>
+            <input type="phone" class="form-control" id="numberPhone">
           </div>
-          <div class="answers-item d-flex justify-content-center">
-            <input type="radio" id="answerItem2" name="answer" class="d-none">
-            <label for="answerItem2" class="d-flex flex-column justify-content-between">
-              <img class="answerImg" src="${images[1]}" alt="burger">
-              <span>${titles[1]}</span>
-            </label>
-          </div>
-            `;
+          `;
+          break;
+        default:
+          nextButton.classList.remove("d-none");
+          prevButton.classList.remove("d-none");
+          sendButton.classList.add("d-none");
+          break;
+      }
     };
 
-    renderAnswers();
+    const renderAnswers = (index) => {
+      questions[index].answers.forEach((answer) => {
+        const answerItem = document.createElement("div");
+
+        answerItem.classList.add(
+          "answers-item",
+          "d-flex",
+          "justify-content-center"
+        );
+
+        answerItem.innerHTML = `
+              <input type="${questions[index].type}" id="answerItem${questions[
+          index
+        ].answers.indexOf(answer)}" name="answer" class="d-none">
+              <label for="answerItem${questions[index].answers.indexOf(
+                answer
+              )}" class="d-flex flex-column justify-content-between">
+                  <img class="answerImg" src="${answer.url}" alt="burger">
+                  <span>${answer.title}</span>
+              </label>
+          `;
+
+        formAnswers.appendChild(answerItem);
+      });
+    };
+
+    const renderQuestions = (indexQuestion) => {
+      formAnswers.innerHTML = "";
+      updateMarkup();
+
+      console.log(indexQuestion, questions.length - 1);
+      if (indexQuestion < questions.length) {
+        questionTitle.textContent = questions[indexQuestion].question;
+        renderAnswers(indexQuestion);
+      } else if (indexQuestion === questions.length + 1) {
+        questionTitle.textContent = "Thank you for your answers!";
+
+        setTimeout(() => {
+          modalBlock.classList.remove("d-block");
+        }, 2000);
+      }
+    };
+
+    renderQuestions(numberQuestion);
+
+    const checkAnswer = () => {
+      const obj = {};
+
+      const inputs = [...formAnswers.elements].filter(
+        (input) => input.checked || input.id === "numberPhone"
+      );
+
+      inputs.forEach((input, index) => {
+        if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+          obj[`${index}_${questions[numberQuestion].question}`] =
+            input.nextElementSibling.textContent.trim();
+        }
+
+        if (numberQuestion === questions.length) {
+          obj["Номер телефона"] = input.value;
+        }
+      });
+
+      finalAnswers.push(obj);
+    };
+
+    nextButton.onclick = () => {
+      checkAnswer();
+      numberQuestion++;
+      renderQuestions(numberQuestion);
+    };
+
+    prevButton.onclick = () => {
+      checkAnswer();
+      numberQuestion--;
+      renderQuestions(numberQuestion);
+    };
+
+    sendButton.onclick = () => {
+      checkAnswer();
+      numberQuestion++;
+      renderQuestions(numberQuestion);
+      console.log(finalAnswers);
+    };
   };
 });
